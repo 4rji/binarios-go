e03752b5de4871c102bf18a8c92b5b3f5f37ed9f
4rji <118249932+4rji@users.noreply.github.com>
2025-05-31 13:49:41 -0600

    Update nmapx.go

--- nmap/nmapx.go
+++ nmap/nmapx.go
@@ -8,19 +8,27 @@ import (
 func main() {
 	// Verificar argumentos
 	if len(os.Args) < 2 {
-		fmt.Println("Usage: ./test_scan_report <CIDR>")
+		fmt.Println("\nUsage: go run nmapx.go utils.go ui.go monitor.go scanner.go report.go <target>")
+		fmt.Println("\n  <target> specifies what to scan. It can be:")
+		fmt.Println("    - A single IP address (e.g. 192.168.1.10)")
+		fmt.Println("    - A CIDR range (e.g. 192.168.1.0/24)")
+		fmt.Println("    - A hostname (e.g. example.com)")
+		fmt.Println("\nExamples:")
+		fmt.Println("  go run nmapx.go utils.go ui.go monitor.go scanner.go report.go 192.168.1.0/24")
+		fmt.Println("  go run nmapx.go utils.go ui.go monitor.go scanner.go report.go 10.0.0.5")
+		fmt.Println("  go run nmapx.go utils.go ui.go monitor.go scanner.go report.go example.com")
 		os.Exit(1)
 	}
 
 	// Configurar la aplicación
 	state := setupUI()
 	state.target = os.Args[1]
-	
+
 	// Iniciar monitores
 	startProcessMonitor(state)
 	startPortsFileMonitor(state)
 	setupOutputRedirection(state)
-	
+
 	// Iniciar escaneo
 	startScan(state)
 
